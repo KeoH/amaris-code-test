581c48c0a0ae3b801130e2010ec856b698ccf0c2
Francisco Manzano <keoh77@gmail.com>
2018-01-08 23:09:22 +0100

    settings

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -8,5 +8,5 @@ ALLOWED_HOSTS = [
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
-MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
+STATIC_ROOT = os.path.join(BASE_DIR, '../../static')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../../media')
